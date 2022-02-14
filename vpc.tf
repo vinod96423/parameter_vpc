a80cf6678fd84d6f16ee771e9047b5078b507077
vinod96423 <92304332+vinod96423@users.noreply.github.com>
2022-02-14 15:59:07 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "myvpc" {
+  cidr_block       = var.vpc_cidr
+  instance_tenancy = "default"
+
+  tags = {
+    Name        = "myvpc
+  }
+}
