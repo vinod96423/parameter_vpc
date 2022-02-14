b8e8487c9f3c64618a89576dee593feaf7028912
vinod96423 <92304332+vinod96423@users.noreply.github.com>
2022-02-14 16:06:22 +0530

    Create web_server.tf

---
+++ web_server.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "myinstance1" {
+    ami                    = var.web_ami
+    subnet_id              = aws_subnet.public1.id
+    vpc_security_group_ids = [aws_security_group.mysecurity1.id]
+    key_name               = "tokyo"
+    instance_type          = "t2.micro"
+    tags = {
+      Name = "instance1"
+    }
+}
+resource "aws_instance" "myinstance2" {
+    ami                    = var.web_ami
+    subnet_id              = aws_subnet.public2.id
+    vpc_security_group_ids = [aws_security_group.mysecurity1.id]
+    key_name               = "tokyo"
+    instance_type          = "t2.micro"
+    tags = {
+      Name = "instance2"
+    }
+}
