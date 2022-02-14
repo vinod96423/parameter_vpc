493e0d780328baf4f56f5ef2acbc0c5e96d85012
vinod96423 <92304332+vinod96423@users.noreply.github.com>
2022-02-14 21:54:53 +0530

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -8,13 +8,4 @@ resource "aws_subnet" "public1" {
     Name = "public1"
   }
 }
-resource "aws_subnet" "public2" {
-  vpc_id                  = aws_vpc.myvpc.id
-  cidr_block              = var.subnet2_cidr
-  map_public_ip_on_launch = "true"
-  availability_zone       = "ap-northeast-1c"
 
-  tags = {
-    Name = "public2"
-  }
-}
