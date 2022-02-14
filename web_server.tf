c18f616c87d4d7ea6c74691da03ae09a8c51a249
vinod96423 <92304332+vinod96423@users.noreply.github.com>
2022-02-14 16:42:43 +0530

    Update web_server.tf

--- web_server.tf
+++ web_server.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "myinstance1" {
     ami                    = var.web_ami
     subnet_id              = aws_subnet.public1.id
     vpc_security_group_ids = [aws_security_group.mysecurity1.id]
-    key_name               = "tokyo"
+    key_name               = var.key_name
     instance_type          = "t2.micro"
     tags = {
       Name = "instance1"
@@ -12,7 +12,7 @@ resource "aws_instance" "myinstance2" {
     ami                    = var.web_ami
     subnet_id              = aws_subnet.public2.id
     vpc_security_group_ids = [aws_security_group.mysecurity1.id]
-    key_name               = "tokyo"
+    key_name               = var.key_name
     instance_type          = "t2.micro"
     tags = {
       Name = "instance2"
